82146b9363e0f2b76ef7349c51e93d3b586a371d
YasunariIguchi <iguchi.yasunari.0219@gmail.com>
2024-09-29 14:04:13 +0900

    chapter6

---
+++ app/page.js
@@ -0,0 +1,10 @@
+const ReadAllItems = () => {
+    return (
+        <div>
+            <h1　className="h1-style">こんにちはだお!</h1>
+            <h3>さよならだお</h3>
+        </div>
+    )
+}
+
+export default ReadAllItems
