cc2d7bb69b994f4333f1d905f30036f0a32c9614
YasunariIguchi <iguchi.yasunari.0219@gmail.com>
2024-10-20 15:02:53 +0900

    8.15

--- app/page.js
+++ app/page.js
@@ -1,8 +1,27 @@
-const ReadAllItems = () => {
+import Link from "next/link"
+import Image from "next/image"
+
+const getAllItems = async() => {
+    const response = await fetch("http://localhost:3000/api/item/readall", {cache: "no-store"})
+    const jsonData = await response.json()
+    const allItems = jsonData.allItems
+    return allItems
+}
+
+const ReadAllItems = async() => {
+    const allItems = await getAllItems()
     return (
         <div>
-            <h1　className="h1-style">こんにちはだお!</h1>
-            <h3>さよならだお</h3>
+            {allItems.map(item =>
+                <Link href="" key={item._id}>
+                    <Image src={item.image} width={750} height={500} alt="item-image" priority></Image>
+                    <div>
+                        <h2>{item.price}</h2>
+                        <h3>{item.title}</h3>
+                        <p>{item.description.substring(0, 80)}...</p>
+                    </div>
+                </Link>
+            )}
         </div>
     )
 }
